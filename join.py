8a389578954810a07bdda306b56a57ad2a446982
Christoph Wagner <christoph@cwagner.me>
2016-04-26 13:52:51 +0200

    Added support for newer join features

--- join.py
+++ join.py
@@ -3,37 +3,33 @@ import urllib.parse
 
 from requests import request
 
-from homeassistant.components.notify import (BaseNotificationService)
-
+from homeassistant.components.notify import (ATTR_TARGET, ATTR_TITLE, BaseNotificationService)
+import pprint
 
 REQUIREMENTS = ['requests']
-# DOMAIN = 'join'
 CONF_DEVICES = 'devices'
 CONF_DEVICE_ID = 'id'
 CONF_DEVICE_NAME = 'name'
 CONF_DEVICE_ICON = 'icon'
-
-ATTR_TARGET = 'device'
-ATTR_ACTION = 'action'
-ATTR_DATA = 'data'
-ATTR_PHONENO = 'phone_number'
 
 API = 'https://joinjoaomgcd.appspot.com/_ah/api/messaging/v1/sendPush?'
 
 
 # noinspection PyUnusedLocal
 def get_service(hass, config):
-    # logger = logging.getLogger(__name__)
-    # devices = config[DOMAIN].get(CONF_DEVICES)
-    #
-    # if not devices:
-    #     logger.error('No device(s) defined')
-    #     return
-    #
-    # if not isinstance(devices, list):
-    #     devices = [devices]
-    devices = None  # the part above is for future dev with an interface
-    return JoinNotificationService(devices)
+    logger = logging.getLogger(__name__)
+    devices = config.get(CONF_DEVICES)
+
+    if not devices:
+        logger.error('No device(s) defined')
+        return
+
+    if not isinstance(devices, list):
+        devices = [devices]
+    device_dict = {}
+    for device in devices:
+        device_dict[device[CONF_DEVICE_NAME]] = device[CONF_DEVICE_ID]
+    return JoinNotificationService(device_dict)
 
 
 def execute(url):
@@ -42,43 +38,51 @@ def execute(url):
 
 
 class JoinNotificationService(BaseNotificationService):
-        
+
     def send_message(self, message, **kwargs):
         logger = logging.getLogger(__name__)
-        logger.warn(message)
-        try:
-            args = message.split('$')
-        except ValueError:
-            logger.error('Invalid target syntax: %s', message)
-            return
+        targets = kwargs.get(ATTR_TARGET)
+        title = kwargs.get(ATTR_TITLE)
+        args = message.split('/', 1)
+        action = args[0]
 
-        if not args[0]:
+        if not targets:
             logger.error('No target defined')
             return
-        if not args[1]:
+        if not action:
             logger.error('No action defined')
             return
-        target = args[0]
-        action = args[1]
-        urls = JoinUrls(target)
+        urls = []
+        for device in targets:
+            urls.append(self.devices.get(device))
+
+        urls = JoinUrls(urls[0]) # TODO allow multiple targets
+        logger.warn(urls)
         if action == 'ring':
             execute(urls.ring())
         else:
-            if not args[2]:
+            if not args[1]:
                 logger.error('No text defined')
                 return
-            data = args[2]
+            data = args[1]
             if action == 'clipboard':
                 execute(urls.set_clipboard(data))
             elif action == 'text':
                 execute(urls.send_text(data))
+            elif action == 'notification':
+                if not title:
+                    logger.error('No title defined')
+                    return
+                execute(urls.set_notification(data,title))
             elif action == 'url':
                 enc_data = urllib.parse.quote_plus(data)
                 execute(urls.open_url(enc_data))
             elif action == 'sms':
+                args = message.split('/', 2)
                 if not args[3]:
                     logger.error('No phone number defined')
                     return
+                data = args[2]
                 phone = args[3]
                 execute(urls.send_sms(data, phone))
             else:
@@ -102,6 +106,9 @@ class JoinUrls:
 
     def open_url(self, url):
         return 'url={1}&deviceId={0}'.format(self.id, url)
+
+    def set_notification(self, data, title):
+        return 'text={1}&title=dd{2}&deviceId={0}'.format(self.id, data, title)
 
     def send_sms(self, text, number):
         return 'smsnumber={1}&smstext={2}&deviceId={0}'.format(self.id, number, text)
